657a66bda5a37ad616c5c964e23596f5759a94ff
Colin <cabmurphrey@gmail.com>
2025-02-14 12:16:58 -0600

    some things

---
+++ navbar/navbar.js
@@ -0,0 +1,8 @@
+(function() {
+
+    // All items we'd like to add
+    var navItems = [
+        {href: '/index.html', text: 'Home'},
+        {href: '/about-me.html', text: 'About me'}
+    ];
+})
